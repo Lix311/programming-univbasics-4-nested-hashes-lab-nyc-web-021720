a24a4a799c568bf690f0e84808bd5a2a9a8c7ca1
Alex Sterrantino <alexsterrantino@gmail.com>
2020-02-05 02:51:48 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -83,9 +83,8 @@ def adding_matz
      }
 
   programmer_hash[:yukihiro_matsumoto] = :yukihiro_matsumoto => {
-#   :known_for => "Ruby",
-#    :languages => ["LISP", "C"]
-  puts programmer_hash
+  :known_for => "Ruby",
+   :languages => ["LISP", "C"]}
 end
 
 def changing_alan
