21c71a608da542f6194e5a17a242c89702e3da54
Alex Sterrantino <alexsterrantino@gmail.com>
2020-02-05 02:49:59 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -15,7 +15,7 @@ def hopper
           :languages => ["C"]
         }
      }
-
+return programmer_hash[:grace_hopper]
 
 end
 
@@ -37,6 +37,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+return programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,6 +56,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+return programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
@@ -80,7 +82,8 @@ def adding_matz
         }
      }
 
-    
+  programmer_hash[:yukihiro_matsumoto]
+  puts programmer_hash
 end
 
 def changing_alan
